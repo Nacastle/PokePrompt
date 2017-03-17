78b2c0b99936944c7c2817fa1c71ca99aa57491d
Bryan Mejia <bryamejia@hotmail.com>
2017-03-17 08:56:10 -0600

    Arbol(?)
    Agrega?

---
+++ BTreeNode.cpp
@@ -0,0 +1,21 @@
+// A BTree node
+class BTreeNode
+{
+	int *keys;
+	int t;
+	BTreeNode **C;
+	int n;
+	bool leaf;
+public:
+	BTreeNode(int _t, bool _leaf);
+
+	void insertNonFull(int k);
+
+	void splitChild(int i, BTreeNode *y);
+
+	void traverse();
+
+	BTreeNode *search(int k);
+
+	friend class BTree;
+};
